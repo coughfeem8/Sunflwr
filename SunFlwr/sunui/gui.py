09a11c5e05039a2ab25890d71ed301dc952cd2c7
Sergio Yanez <seryan.1115@gmail.com>
2019-06-29 23:16:11 -0600

    added main gui starter

--- SunFlwr/sunui/gui.py
+++ SunFlwr/sunui/gui.py
@@ -1,5 +1,23 @@
-from tkinter import Tk, ttk
-root = Tk()
-main_win = ttk.Frame(root)
-root.title("SunFlwr")
-root.mainloop()
+from tkinter import ttk
+import tkinter as tk
+from PIL import ImageTk, Image
+
+
+class SunFlwrGUI(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        Tk.__init__(self, *args, *kwargs)
+        self.title("SunFlwr")
+
+        root = ttk.Frame(self).grid(column=0, row=0, sticky='nswe')
+
+        sections = ['Boot-Up Page', 'Start Window', 'New Project',
+                    'Evaluation Preparation',
+                    'Public Services And Urban Equipments', 'Land',
+                    'Urban Characteristics And Building Description',
+                    'Construction Elements', 'Elements And Accesories',
+                    'Preview', 'Evaluation Created']
+
+
+if __name__ == "__main__":
+    app = SunFlwrGUI()
+    app.mainloop()
